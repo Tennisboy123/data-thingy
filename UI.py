5170242d2244171bb4ba11f17165622aeca564e1
Seb <162109233+Tennisboy123@users.noreply.github.com>
2024-08-15 10:15:49 +1000

    Update main.py and UI.py
    In main.py, I changed it so that .csv file only shows the delayed flights. After a lot of testing, it worked, and was successful. in UI.py, is started making the different categories for which the data will be displayed, like "most_delayed_airline" and "most_delayed_time_block". Next, I need to continue this and finish the user interface.

--- UI.py
+++ UI.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 quit = False
 
 airplane_delays_df = pd.read_csv('Data/airplane_delays.csv')
-print(airplane_delays_df)
+
+show_most_delayed_time_block = airplane_delays_df['DEP_TIME_BLK'].mode()
+#print(show_most_delayed_time_block)
+
+show_most_delayed_airline = airplane_delays_df['CARRIER_NAME'].mode()
+#print("The most delayed airline in 2019 was " + show_most_delayed_airline)
+
+
     
-def most_delayed_airline():
-    if airplane_delays_df['DEP_DEL15'] == 1:
-        return airplane_delays_df['DEP_DEL15']
-    else:
-        airplane_delays_df=airplane_delays_df['DEP_DEL15'].drop
-        
-most_delayed_airline()
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 airplane_delays_df = pd.read_csv('data/full_data_flightdelay.csv', on_bad_lines='warn')
-print(airplane_delays_df)
 
 airplane_delays_df = airplane_delays_df.drop(columns=['DISTANCE_GROUP',
                                                       'SEGMENT_NUMBER',
@@ -17,11 +16,13 @@ airplane_delays_df = airplane_delays_df.drop(columns=['DISTANCE_GROUP',
                                                       'LATITUDE',
                                                       'LONGITUDE',
                                                       'PREVIOUS_AIRPORT'])
-print(airplane_delays_df)
 
 airplane_delays_df['PRCP_CM'] = airplane_delays_df.PRCP * 2.54
 airplane_delays_df['SNOW_CM'] = airplane_delays_df.SNOW * 2.54
 airplane_delays_df['TMAX_CELSIUS'] = (airplane_delays_df.TMAX-32) * 5/9
+
+airplane_delays_df = airplane_delays_df[airplane_delays_df['DEP_DEL15'] == 1]
 print(airplane_delays_df)
+
 
 airplane_delays_df.to_csv('airplane_delays.csv', index=False)
